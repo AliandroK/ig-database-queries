35c11c41a4ea921dc0f34c176d37a53d5819b06b
AliandroK <aliandroklug@gmail.com>
2023-01-21 16:41:38 -0300

    adding functionalities to users and games repositories

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,4 +1,4 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository, ReturningStatementNotSupportedError } from 'typeorm';
 
 import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
 import { User } from '../../entities/User';
@@ -15,16 +15,26 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    
+    const user = (await this.repository
+                 .findOne({
+                   relations:["games"],
+                   where: {id: user_id}}
+                   )) as User ;
+     
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("select * from users order by first_name asc "); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository
+    .query("select * from users where LOWER(users.first_name) = LOWER($1) and LOWER(users.last_name) = LOWER($2) ", 
+    [first_name, last_name]); // Complete usando raw query
   }
 }
